b975e789db010defdb077c8ddfd2d2d0a5366dc2
mosahle7 <sahlethappi@gmail.com>
2024-01-08 19:21:14 +0530

    2nd Commit

--- src/server.js
+++ src/server.js
@@ -4,11 +4,21 @@ import {StatusCodes} from 'http-status-codes';
 import userRoutes from './user.routes';
 import mainRoutes from './main.routes';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import compression from 'compression';
 
 const app = express();
 const port = 3000;
 
-
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 1 minute
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	// standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	// legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// // store: ... , // Use an external store for consistency across multiple server instances.
+})
+app.use(compression());
+app.use(limiter)
 app.use(express.json());
 app.use(helmet());
 
